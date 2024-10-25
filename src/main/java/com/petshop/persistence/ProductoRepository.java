dd16eb79de535f0f0daeba71ee2d39716cb3f3c3
perdomomedina.carlos@gmail.com <perdomomedina.carlos@gmail.com>
2024-10-25 14:10:22 -0500

    metodo update producto

--- src/main/java/com/petshop/persistence/ProductoRepository.java
+++ src/main/java/com/petshop/persistence/ProductoRepository.java
@@ -19,4 +19,27 @@ public class ProductoRepository {
         return productoCrudRepository.findById(id);
     }
 
+    public Producto saveProduct(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+
+    public Producto updateProduct(Producto producto) {
+        Optional<Producto> existProduct = productoCrudRepository.findById(producto.getProductoID());
+        if (existProduct.isPresent()) {
+            existProduct.get().setNombre(producto.getNombre());
+            existProduct.get().setCantidadStock(producto.getCantidadStock());
+            existProduct.get().setCategoriaID(producto.getCategoriaID());
+            existProduct.get().setEstado(producto.isEstado());
+            existProduct.get().setCodigoBarras(producto.getCodigoBarras());
+            existProduct.get().setPrecioVenta(producto.getPrecioVenta());
+            return productoCrudRepository.save(existProduct.get());
+        }
+
+        return productoCrudRepository.save(producto);
+    }
+
+    public void deleteProduct(int id) {
+        productoCrudRepository.deleteById(id);
+    }
+
 }
